545b5c669692538ddf8488e18707ed9b7a89c397
HridaySarder <hridoyaug12@gmail.com>
2024-05-22 20:06:30 +0600

    added dynamic object

---
+++ dynamic_object.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+    Student(int roll, int cls, double gpa)
+    {
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+Student *fun()
+{
+    Student *imam = new Student(520, 58, 5.88);
+    return imam;
+}
+int main()
+{
+    Student *ans = fun();
+    cout << ans->roll << " " << ans->cls << " " << ans->gpa << endl;
+    delete ans;
+    cout << ans->roll << " " << ans->cls << " " << ans->gpa << endl;
+    return 0;
+}
